b7541d1dc1500485f554ffca0089e832c92ca09a
Carl Hofmeister <hofmeister.carl@gmail.com>
2019-12-20 00:36:56 -0600

    Added goto command
    Only supported via the shell plugin

--- manor.plugin.zsh
+++ manor.plugin.zsh
@@ -1,9 +1,16 @@
 #!/usr/bin/env zsh
 
-MANOR_INSTALL_LOCATION=$(dirname $(readlink -f $0))/src
+if [[ -z $MANOR_INSTALL_LOCATION ]]; then
+    MANOR_INSTALL_LOCATION=$(dirname $(readlink -f $0))/src
+fi
 
 function manor() {
-    $MANOR_INSTALL_LOCATION/manor $@
+    if [[ $1 == 'goto' ]]; then
+        source $MANOR_INSTALL_LOCATION/util.sh
+        cd $MODULES_DIR/$(name_to_dir $2)
+    else
+        $MANOR_INSTALL_LOCATION/manor $@
+    fi
 }
 
 fpath+="${0:h}/completion"
---
+++ src/util.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+########### Constants ###########
+if [[ -z $MODULES_DIR ]]; then
+    MODULES_DIR="$XDG_DATA_HOME/manor"
+fi
+MANOR_DEFAULT_MODULE_LIST="enabled_modules"
+if [[ -z $MODULES_LIST ]]; then
+    MODULES_LIST="$MODULES_DIR/$MANOR_DEFAULT_MODULE_LIST"
+fi
+
+########## Utility functions #############
+function ensure_modules_dir_exists() {
+    if [[ ! -d $MODULES_DIR ]]; then
+        mkdir $MODULES_DIR
+    fi
+}
+
+function url_to_name() {
+    url=$1
+    if echo $url | grep -q 'git@.*\.git' - ; then
+        name=`echo $url | cut -d '/' -f 2 | sed 's/.git//'`
+        echo $name
+    else
+        echo $url
+    fi
+}
+
+function name_to_git_url() {
+    name=$1
+    printf "git@github.com:%s.git" $name
+}
+
+function name_to_dir() {
+    name=$1
+    echo $name | sed -r 's/.*\///'
+}
+
+function read_modules_list() {
+    if [[ -f $MODULES_LIST ]]; then
+        echo $(grep -v "^#.*" $MODULES_LIST) | sed 's/"//g'
+    elif [[ "$(declare -p MODULES_LIST)" =~ "declare -a" ]]; then
+        echo ${MODULES_LIST[@]} | sed 's/"//g"'
+    else
+        echo "MODULES_LIST: $MODULES_LIST does not exist!"
+        exit 1
+    fi
+}
